73c585ab558920bb11b20b164ad9e1cbfabf1811
IDurgeshShukla <shukladurgesh4545@gmail.com>
2023-07-15 12:42:56 +0530

    created constructor for boat

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -2,6 +2,10 @@ package com.driver;
 public class Boat implements WaterVehicle {
     private String name;
     private int capacity;
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
     public String getVehicleName() {
         return this.name;
     }
